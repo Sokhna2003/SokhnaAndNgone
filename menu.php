92bcebb4b4cf09eebe8155367b777bebb9331911
sokhna <dianesokhna2003@gmail.com>
2026-02-20 22:02:16 +0000

    amelioration fonction soustraction

--- menu.php
+++ menu.php
@@ -1,9 +1,11 @@
 <?php
+$addition = include 'addition.php'; 
+$soustraction = include 'soustraction.php'; 
+$multiplication = include 'multiplication.php'; 
+$division = include 'division.php'; 
 include 'calculatrice.php'; 
-include 'addition.php';// ou require_once
 
 // Vous pouvez maintenant utiliser addition() ici
-
 
 function Menu() : void{
   echo "====== Menu ========\n";
@@ -24,10 +26,26 @@ do{
   switch ($choix){
     case 1:
     // PHP comprend qu'il doit chercher une fonction nommée "addition"
-    echo "Résultat : " . calculer(10, 5, 'addition'); 
+    echo "Résultat : " . calculer($a, $b, $addition)."\n"; 
     break;
-
-   
+    case 2:
+    // PHP comprend qu'il doit chercher une fonction nommée "soustraction"
+    echo "Résultat : " . calculer($a, $b, $soustraction)."\n"; 
+    break;
+    case 3:
+    // PHP comprend qu'il doit chercher une fonction nommée "multiplication"
+    echo "Résultat : " . calculer($a, $b, $multiplication)."\n"; 
+    break;
+    case 4:
+    // PHP comprend qu'il doit chercher une fonction nommée "division"
+    echo "Résultat : " . calculer($a, $b, $division)."\n"; 
+    break;
+    case 5:
+    // PHP comprend qu'il doit chercher une fonction nommée "addition"
+    echo "Au revoir \n"; 
+    break;
+    default:
+    echo "Choix invalide \n";
       
   }
 }while($choix!=5);
